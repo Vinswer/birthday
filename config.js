48006a7983e4078831882814dde5f91654535143
Vinswer <152801057+Vinswer@users.noreply.github.com>
2023-12-04 00:40:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,11 +30,11 @@ var config = {
     desc: {
         turn_on: "Start",
         play: "Music!",
-        #bannar_coming: "颜色",
+ 
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "老公生日快乐！！！",
-        #story: "A MESSAGE FOR YOU",
+
     }
 };
